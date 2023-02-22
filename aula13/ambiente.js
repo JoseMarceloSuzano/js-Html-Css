cbd99e0b3cbc9f80ce9d8bfbdee379a591cd3b70
marcellinjr02@gmail.com <marcellinjr02@gmail.com>
2023-02-22 17:35:05 -0300

    Primeiro Commit

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 2)
+
+/*
+var contador = 1
+while (contador <= 6) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+*/
